6990ce6d51d1ba240616a98a9d954b985515b968
TinaTiel <51816935+TinaTiel@users.noreply.github.com>
2020-11-12 22:17:44 +0100

    Comitting updates from lesson.

---
+++ src/main/java/guru/config/GreetingConfig.java
@@ -0,0 +1,41 @@
+package guru.config;
+
+import guru.services.GreetingRepository;
+import guru.services.GreetingService;
+import guru.services.GreetingServiceFactory;
+import guru.services.PrimaryGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository greetingRepository) {
+        return new GreetingServiceFactory(greetingRepository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"en", "default"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+    @Bean
+    @Profile("es")
+    @Primary
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+}
--- src/main/java/guru/services/GreetingServiceImpl.java
+++ src/main/java/guru/services/GreetingServiceImpl.java
@@ -1,5 +1,6 @@
 package guru.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 /**
